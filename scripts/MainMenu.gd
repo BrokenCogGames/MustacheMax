6ef60b684dc0fd6570e8b3d642d48eb4976f7d5b
Bryant Hayes <bryantfhayes@gmail.com>
2020-05-13 23:37:20 -0700

    Add Menus

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_PlayButton_pressed():
 	pass # Replace with function body.
 
+func _on_OptionButton_pressed():
+	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_LeaderboardButton_pressed():
+	pass # Replace with function body.
